f37c25f9f53a84505822e1e9fea290200e3077c5
Sarah Arshad <sarshad1@yahoo.co.uk>
2022-05-26 10:47:27 +0100

    completed restaurant and reviews challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning Database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample # has to meet validation criteria to get saved to db
+  )
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished creating restaurants!'
